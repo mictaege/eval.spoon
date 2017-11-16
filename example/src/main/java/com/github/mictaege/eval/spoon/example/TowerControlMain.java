e6aa87f23c9f16fe50f240709fa75f84a91ce862
Michael Täge <mictaege@googlemail.com>
2017-11-16 12:06:58 +0100

    Update spoon-gradle-plugin v1.2

---
+++ example/src/main/java/com/github/mictaege/eval/spoon/example/TowerControlMain.java
@@ -0,0 +1,10 @@
+package com.github.mictaege.eval.spoon.example;
+
+import javafx.application.Application;
+
+public class TowerControlMain {
+
+    public static void main(String[] args) {
+        Application.launch(TowerControlApp.class, args);
+    }
+}
